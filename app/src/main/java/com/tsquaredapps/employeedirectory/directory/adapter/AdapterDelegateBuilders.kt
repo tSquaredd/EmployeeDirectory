0269be70edb79e180fbfb6b7f199533578a2ba0f
Tyler Turnbull <tylerturnbull13@gmail.com>
2020-08-30 14:23:09 -0400

    Force Picasso to only get image from network if not in cache

--- app/src/main/java/com/tsquaredapps/employeedirectory/directory/adapter/AdapterDelegateBuilders.kt
+++ app/src/main/java/com/tsquaredapps/employeedirectory/directory/adapter/AdapterDelegateBuilders.kt
@@ -1,11 +1,14 @@
 package com.tsquaredapps.employeedirectory.directory.adapter
 
 import com.hannesdorfmann.adapterdelegates4.dsl.adapterDelegateViewBinding
+import com.squareup.picasso.Callback
+import com.squareup.picasso.NetworkPolicy
 import com.squareup.picasso.Picasso
 import com.tsquaredapps.employeedirectory.R
 import com.tsquaredapps.employeedirectory.databinding.EmployeeItemBinding
 import com.tsquaredapps.employeedirectory.databinding.LetterHeaderItemBinding
 import com.tsquaredapps.employeedirectory.model.Employee
+import java.lang.Exception
 
 fun employeeAdapterDelegate(itemClickListener: (Employee) -> Unit) =
     adapterDelegateViewBinding<Employee, DirectoryScreenModel, EmployeeItemBinding>(
@@ -17,11 +20,24 @@ fun employeeAdapterDelegate(itemClickListener: (Employee) -> Unit) =
                 name.text = item.fullName
                 team.text = item.team
 
-                Picasso.get()
-                    .load(item.smallPhotoUrl)
-                    .placeholder(R.drawable.placeholder_person)
-                    .error(R.drawable.placeholder_person)
-                    .into(image)
+                with(Picasso.get()) {
+                    load(item.smallPhotoUrl)
+                        .networkPolicy(NetworkPolicy.OFFLINE)
+                        .placeholder(R.drawable.placeholder_person)
+                        .error(R.drawable.placeholder_person)
+                        .into(image, object : Callback {
+                            override fun onSuccess() {
+                                // no-op, loaded from cache
+                            }
+
+                            override fun onError(e: Exception?) {
+                                load(item.smallPhotoUrl)
+                                    .placeholder(R.drawable.placeholder_person)
+                                    .error(R.drawable.placeholder_person)
+                                    .into(image)
+                            }
+                        })
+                }
             }
         }
     }
